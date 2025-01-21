1e9998f56d6891340d12c21375ab08163acb09be
Wassim Baratli <wassim.baratli@etu.univ-cotedazur.fr>
2025-01-21 23:37:53 +0100

    Feature Double sélection

--- Assets/CubeSelector.cs
+++ Assets/CubeSelector.cs
@@ -5,15 +5,38 @@ using UnityEngine;
 public class CubeSelector : MonoBehaviour
 {
     private Renderer cubeRenderer;
+    private bool isSelected = false;
 
     void Start()
     {
         cubeRenderer = GetComponent<Renderer>();
+        cubeRenderer.material.color = Color.red;
+
     }
 
     private void OnMouseDown()
     {
-        cubeRenderer.material.color = Color.green;
-        Debug.Log($"Cube sélectionné : {gameObject.name}");
+       // Si le cube est sélectionné, on le désélectionne
+        if (isSelected==true)
+        {
+            isSelected = false;
+            cubeRenderer.material.color = Color.red;
+            SelectionManager.Instance.DeselectRoom(this.transform);
+            Debug.Log($"Cube désélectionné : {gameObject.name}");
+        }
+        // Sinon, on essaye de le sélectionner
+        else
+        {
+            if (SelectionManager.Instance.SelectRoom(this.transform))
+            {
+                isSelected = true;
+                cubeRenderer.material.color = Color.blue;
+                Debug.Log($"Cube sélectionné : {gameObject.name}");
+            }
+            else
+            {
+                Debug.Log("Impossible de sélectionner plus de 2 salles.");
+            }
+        }
     }
 }
---
+++ Assets/SelectionManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SelectionManager : MonoBehaviour
+{
+    
+   public static SelectionManager Instance;
+
+    [SerializeField] private TextMeshProUGUI feedbackText; // Texte pour afficher le temps/distance
+    private List<Transform> selectedRooms = new List<Transform>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Sélectionne une salle
+    public bool SelectRoom(Transform room)
+    {
+        if (selectedRooms.Count<2  && !selectedRooms.Contains(room)) // Limiter à 2 sélections
+        {
+            selectedRooms.Add(room);
+            Debug.Log($"Salle ajoutée : {room.name}");
+
+            if (selectedRooms.Count == 2)
+            {
+                CalculateTimeOrDistance();
+            }
+
+            return true; // Sélection réussie
+        }
+        else
+        {
+            Debug.Log("Vous ne pouvez sélectionner que 2 salles à la fois.");
+            return false; // Sélection échouée
+        }
+    }
+
+    // Désélectionne une salle
+    public void DeselectRoom(Transform room)
+    {
+        if (selectedRooms.Contains(room))
+        {
+            selectedRooms.Remove(room);
+            Debug.Log($"Salle retirée : {room.name}");
+            
+            if (selectedRooms.Count < 2)
+            {
+                feedbackText.text = "";
+            }
+        }
+    }
+
+    // Calcule et affiche le temps ou la distance entre les deux salles
+    private void CalculateTimeOrDistance()
+    {
+        if (selectedRooms.Count == 2)
+        {
+            Transform room1 = selectedRooms[0];
+            Transform room2 = selectedRooms[1];
+
+            // Calculer la distance entre les deux salles
+            float distance = Vector3.Distance(room1.position, room2.position);
+            float estimatedTime = distance * 2; // Exemple : 2 secondes par unité de distance
+
+            // Afficher le résultat
+            feedbackText.text = $"Temps estimé : {estimatedTime:F1} minutes";
+            Debug.Log($"Distance entre {room1.name} et {room2.name} : {distance:F1}");
+        }
+    }
+}
